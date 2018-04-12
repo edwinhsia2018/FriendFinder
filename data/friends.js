3843011ae0b27f2be1b36b528c36f035beef5ce3
Edwin Hsia <edwinhsia@gmail.com>
2018-04-12 14:26:08 -0500

    wednesday updates

--- data/friends.js
+++ data/friends.js
@@ -2,16 +2,17 @@ var friendArray = [
   {
     name: "Friend",
     link: "image.google.com",
-    q1: 5,
-    q2: 5,
-    q3: 5,
-    q4: 5,
-    q5: 5,
-    q6: 5,
-    q7: 5,
-    q8: 5,
-    q9: 5,
-    q10: 5
+    scores: [5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
   }
 ];
 
